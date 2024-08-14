9de6f2bfea3ecb5e1b0b2a2950c9ec4e35ed7d1e
Anton Do <antonhdo@gmail.com>
2024-08-13 22:38:49 -0700

    Head updated?

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import Layout from './components/Layout';
+import Head from "next/head";
+import React from "react";
+import Layout from "./components/Layout";
 
 const App = () => {
-  return <Layout />;
+  return (
+    <div>
+      <div>
+        <Head>
+          <link rel="shortcut icon" href="src\app\page.js" />
+        </Head>
+      </div>
+      <div>
+        <Layout />;
+      </div>
+    </div>
+  );
 };
 
 export default App;
